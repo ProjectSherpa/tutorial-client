06384fbf8e5303d1f62f8405d645598c91ce076c
Quin Kinser <quinkinser@gmail.com>
2016-08-04 22:53:18 -0700

    Set up material-ui and add sample nav bar

--- src/app.js
+++ src/app.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import NavBar from './NavBar';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
  
+const App = () => (
+  <MuiThemeProvider>
+    <NavBar />
+  </MuiThemeProvider>
+);
+
 document.addEventListener('DOMContentLoaded', function() {
   ReactDOM.render(
-    <NavBar />,
+    <App />,
     document.getElementById('mount')
   );
 });
