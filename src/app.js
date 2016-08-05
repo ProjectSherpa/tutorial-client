3cd29f295cbd6581d1a4ef7f1eef4476bf50c6bb
Quin Kinser <quinkinser@gmail.com>
2016-08-04 21:54:28 -0700

    Fix bug by renaming component

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Counter from './Counter';
+import NavBar from './NavBar';
  
 document.addEventListener('DOMContentLoaded', function() {
   ReactDOM.render(
