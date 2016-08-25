86133f5725f5c2d0b03f5863977e34e69f616eb4
Jeremy Toce <jeremytoce@gmail.com>
2016-08-24 21:44:46 -0700

    significant top bar progress

--- src/ContentContainer.js
+++ src/ContentContainer.js
@@ -1,13 +1,23 @@
 import React, { PropTypes } from 'react'
 import {DropdownButton, MenuItem, ButtonGroup, Button, Col} from 'react-bootstrap';
+import {Link} from 'react-router';
 
 export default function ContentContainer (props) {
+
   return (
+
     <div>
-      <ButtonGroup justified>
-         <Button className="prev-next-buttons" href="#"><span className="glyphicon glyphicon-menu-left" aria-hidden="true"></span>   Previous Lesson</Button>
-         <Button className="prev-next-buttons" href="#">Next Lesson   <span className="glyphicon glyphicon-menu-right" aria-hidden="true"></span></Button>
-       </ButtonGroup>
+
+      <Link to={props.lessonContainer[props.currentLesson-1]}>
+        <Button className="prev-next-buttons" onClick={() => props.setCurrentLesson(props.currentLesson-1)}> 
+        <span className="glyphicon glyphicon-menu-left" aria-hidden="true"> </span>   
+			Previous Lesson
+		</Button>
+      </Link>
+        <Button className="prev-next-buttons">
+         	Next Lesson   
+        <span className="glyphicon glyphicon-menu-right" aria-hidden="true"></span>
+        </Button>
     </div>
   )
 }
--- src/SideBar.js
+++ src/SideBar.js
@@ -80,31 +80,9 @@ export default class SideBar extends React.Component {
     }
   }
 
-  findUser(users, email){
-    // search through users to find desired user
-
-    // store user to state
-
-  }
-
-  // getUsers(){
-  //   console.log("Inside getUsers right now");
-  //   fetch('http://private-a5d61-sherpaapi.apiary-mock.com/users', {
-  //     method: 'GET',
-  //     headers: {'content-type': 'application/json'},
-  //   })
-  //   .then(res => res.json())
-  //   .then(users => {
-  //     console.log(users)
-  //     users.reduce(function(a, b) {
-  //       if (b.email === this  
-  //     }, false)
-
-  //   });
-  // }
 
   markComplete(lid) {
-    console.log("lesson completed!")
+    console.log(this.props);
     event.preventDefault()
     var lessons = this.state.user.lessonsCompleted;
     //var el = event.target
@@ -119,12 +97,14 @@ export default class SideBar extends React.Component {
     // }
     this.displayLessons();
     this.calculateCompletion();
+    this.props.setCurrentLesson(lid);
+    
  
 
   }
   
   render() {
-    console.log("COMPETION %%", this.state.completionPercentage)
+    console.log("COMPLETION %%", this.state.completionPercentage)
     return (
       <div className="sidebar-container">
       	<ButtonGroup className="scroll" vertical block>
@@ -145,13 +125,13 @@ export default class SideBar extends React.Component {
          
                 
               <Link to="/basecamp/welcome">
-                <Button id="lid1" onClick={() => this.markComplete("lid1")}  className={this.state.lessons.lid1}><FontAwesome name="video-camera" className="indent"/>   Welcome</Button>
+                <Button id="lid1" onClick={() => this.markComplete(1)}  className={this.state.lessons.lid1}><FontAwesome name="video-camera" className="indent"/>   Welcome</Button>
               </Link> 
               <Link to="/basecamp/resources">
-                <Button id="lid2" onClick={() => this.markComplete("lid2")} className={this.state.lessons.lid2}><FontAwesome name="file-text" className="indent"/>   Intro Resources</Button>
+                <Button id="lid2" onClick={() => this.markComplete(2)} className={this.state.lessons.lid2}><FontAwesome name="file-text" className="indent"/>   Intro Resources</Button>
               </Link> 
               <Link to="/basecamp/note">
-                <Button id="lid3" onClick={() => this.markComplete("lid3")} className={this.state.lessons.lid3}><FontAwesome name="file-text" className="indent"/>   A Note Before You Begin</Button>
+                <Button id="lid3" onClick={() => this.markComplete(3)} className={this.state.lessons.lid3}><FontAwesome name="file-text" className="indent"/>   A Note Before You Begin</Button>
               </Link> 
             </ButtonGroup>
           </Panel>
@@ -159,22 +139,22 @@ export default class SideBar extends React.Component {
           <Panel collapsible defaultExpanded header="Getting Started" className="panel-headings">
             <ButtonGroup fill vertical block>  
               <Link to="/gettingstarted/resources">
-                <Button id="lid4" onClick={() => this.markComplete("lid4")} className={this.state.lessons.lid4}><FontAwesome name="file-text" className="indent"/>   Resources</Button>
+                <Button id="lid4" onClick={() => this.markComplete(4)} className={this.state.lessons.lid4}><FontAwesome name="file-text" className="indent"/>   Resources</Button>
               </Link>
               <Link to="/gettingstarted/intro">
-                <Button id="lid5" onClick={() => this.markComplete("lid5")} className={this.state.lessons.lid5}><FontAwesome name="video-camera" className="indent"/>   Introduction to System Architecture</Button>
+                <Button id="lid5" onClick={() => this.markComplete(5)} className={this.state.lessons.lid5}><FontAwesome name="video-camera" className="indent"/>   Introduction to System Architecture</Button>
               </Link>
               <Link to="/gettingstarted/codealong1">
-                <Button id="lid6" onClick={() => this.markComplete("lid6")} className={this.state.lessons.lid6}><FontAwesome name="video-camera" className="indent"/>   Code Along 1</Button>
+                <Button id="lid6" onClick={() => this.markComplete(6)} className={this.state.lessons.lid6}><FontAwesome name="video-camera" className="indent"/>   Code Along 1</Button>
               </Link>
               <Link to="/gettingstarted/codealong2">
-                <Button id="lid7" onClick={() => this.markComplete("lid7")} className={this.state.lessons.lid7}><FontAwesome name="video-camera" className="indent"/>   Code Along 2</Button>
+                <Button id="lid7" onClick={() => this.markComplete(7)} className={this.state.lessons.lid7}><FontAwesome name="video-camera" className="indent"/>   Code Along 2</Button>
               </Link>
               <Link to="/gettingstarted/codealong3">
-                <Button id="lid8" onClick={() => this.markComplete("lid8")} className={this.state.lessons.lid8}><FontAwesome name="video-camera" className="indent"/>   Code Along 3</Button>
+                <Button id="lid8" onClick={() => this.markComplete(8)} className={this.state.lessons.lid8}><FontAwesome name="video-camera" className="indent"/>   Code Along 3</Button>
               </Link>
               <Link to="/gettingstarted/wrapup">
-                <Button id="lid9" onClick={() => this.markComplete("lid9")} className={this.state.lessons.lid9}><FontAwesome name="file-text" className="indent"/>   Wrap up</Button>
+                <Button id="lid9" onClick={() => this.markComplete(9)} className={this.state.lessons.lid9}><FontAwesome name="file-text" className="indent"/>   Wrap up</Button>
               </Link>   
             </ButtonGroup>
           </Panel>
@@ -182,19 +162,19 @@ export default class SideBar extends React.Component {
           <Panel collapsible defaultExpanded header="Scaling Vertically" className="panel-headings">
             <ButtonGroup fill vertical block>  
               <Link to="/vertical/resources">
-                <Button id="lid10" onClick={() => this.markComplete("lid10")} className={this.state.lessons.lid10}><FontAwesome name="file-text" className="indent" />   Resources</Button>
+                <Button id="lid10" onClick={() => this.markComplete(10)} className={this.state.lessons.lid10}><FontAwesome name="file-text" className="indent" />   Resources</Button>
               </Link>
               <Link to="/vertical/overview">
-                <Button id="lid11" onClick={() => this.markComplete("lid11")} className={this.state.lessons.lid11}><FontAwesome name="video-camera" className="indent"/>   Overview</Button>
+                <Button id="lid11" onClick={() => this.markComplete(11)} className={this.state.lessons.lid11}><FontAwesome name="video-camera" className="indent"/>   Overview</Button>
               </Link>
               <Link to="/vertical/action">
-                <Button id="lid12" onClick={() => this.markComplete("lid12")} className={this.state.lessons.lid12}><FontAwesome name="video-camera" className="indent" />   Action</Button>
+                <Button id="lid12" onClick={() => this.markComplete(12)} className={this.state.lessons.lid12}><FontAwesome name="video-camera" className="indent" />   Action</Button>
               </Link>
               <Link to="/vertical/challenge">
-                <Button id="lid13" onClick={() => this.markComplete("lid13")} className={this.state.lessons.lid13}><FontAwesome name="video-camera" className="indent" />   Scaling Challenge</Button>
+                <Button id="lid13" onClick={() => this.markComplete(13)} className={this.state.lessons.lid13}><FontAwesome name="video-camera" className="indent" />   Scaling Challenge</Button>
               </Link>
               <Link to="/vertical/wrapup">
-                <Button id="lid14" onClick={() => this.markComplete("lid14")} className={this.state.lessons.lid14}><FontAwesome name="video-camera" className="indent" />   Wrap up</Button>
+                <Button id="lid14" onClick={() => this.markComplete(14)} className={this.state.lessons.lid14}><FontAwesome name="video-camera" className="indent" />   Wrap up</Button>
               </Link>
             </ButtonGroup>
           </Panel>
@@ -202,19 +182,19 @@ export default class SideBar extends React.Component {
           <Panel collapsible defaultExpanded header="Isolation of Services" className="panel-headings">
             <ButtonGroup fill vertical block>  
               <Link to="/isolation/resources">
-                <Button id="lid15" onClick={() => this.markComplete("lid15")} className={this.state.lessons.lid15}><FontAwesome name="file-text" className="indent" />   Resources(+docker videos)</Button>
+                <Button id="lid15" onClick={() => this.markComplete(15)} className={this.state.lessons.lid15}><FontAwesome name="file-text" className="indent" />   Resources(+docker videos)</Button>
               </Link>
               <Link to="/isolation/overview">
-                <Button id="lid16" onClick={() => this.markComplete("lid16")} className={this.state.lessons.lid16}><FontAwesome name="video-camera" className="indent"/>   Overview</Button>
+                <Button id="lid16" onClick={() => this.markComplete(16)} className={this.state.lessons.lid16}><FontAwesome name="video-camera" className="indent"/>   Overview</Button>
               </Link>
               <Link to="/isolation/scenario">
-                <Button id="lid17" onClick={() => this.markComplete("lid17")} className={this.state.lessons.lid17}><FontAwesome name="gamepad" className="indent" />   Gameday</Button>
+                <Button id="lid17" onClick={() => this.markComplete(17)} className={this.state.lessons.lid17}><FontAwesome name="gamepad" className="indent" />   Gameday</Button>
               </Link>
               <Link to="/isolation/solution">
-                <Button id="lid18" onClick={() => this.markComplete("lid18")} className={this.state.lessons.lid18}><FontAwesome name="video-camera" className="indent"/>   Code Solution</Button>
+                <Button id="lid18" onClick={() => this.markComplete(18)} className={this.state.lessons.lid18}><FontAwesome name="video-camera" className="indent"/>   Code Solution</Button>
               </Link>
               <Link to="/isolation/wrapup">
-                <Button id="lid19" onClick={() => this.markComplete("lid19")} className={this.state.lessons.lid19}><FontAwesome name="video-camera" className="indent" />   Wrap up</Button>
+                <Button id="lid19" onClick={() => this.markComplete(19)} className={this.state.lessons.lid19}><FontAwesome name="video-camera" className="indent" />   Wrap up</Button>
               </Link>
             </ButtonGroup>
           </Panel>
@@ -223,22 +203,22 @@ export default class SideBar extends React.Component {
           <Panel collapsible defaultExpanded header="Optimizing Content Delivery" className="panel-headings">
             <ButtonGroup fill vertical block>  
               <Link to="/content/resources">
-                <Button id="lid20" onClick={() => this.markComplete("lid09")} className={this.state.lessons.lid20}><FontAwesome name="file-text" className="indent" />   Resources</Button>
+                <Button id="lid20" onClick={() => this.markComplete(20)} className={this.state.lessons.lid20}><FontAwesome name="file-text" className="indent" />   Resources</Button>
               </Link>
               <Link to="/content/overview">
-                <Button id="lid21" onClick={() => this.markComplete("lid21")} className={this.state.lessons.lid21}><FontAwesome name="video-camera" className="indent"/>   Overview</Button>
+                <Button id="lid21" onClick={() => this.markComplete(21)} className={this.state.lessons.lid21}><FontAwesome name="video-camera" className="indent"/>   Overview</Button>
               </Link>
               <Link to="/content/scenario">
-                <Button id="lid22" onClick={() => this.markComplete("lid22")} className={this.state.lessons.lid22}><FontAwesome name="gamepad" className="indent" />   Gameday</Button>
+                <Button id="lid22" onClick={() => this.markComplete(22)} className={this.state.lessons.lid22}><FontAwesome name="gamepad" className="indent" />   Gameday</Button>
               </Link>
               <Link to="/content/solution1">
-                <Button id="lid23" onClick={() => this.markComplete("lid23")} className={this.state.lessons.lid23}><FontAwesome name="video-camera" className="indent" />   S3 + Display</Button>
+                <Button id="lid23" onClick={() => this.markComplete(23)} className={this.state.lessons.lid23}><FontAwesome name="video-camera" className="indent" />   S3 + Display</Button>
               </Link>
               <Link to="/content/solution2">
-                <Button id="lid24" onClick={() => this.markComplete("lid24")} className={this.state.lessons.lid24}><FontAwesome name="video-camera" className="indent" />   S3 + Cloudfront</Button>
+                <Button id="lid24" onClick={() => this.markComplete(24)} className={this.state.lessons.lid24}><FontAwesome name="video-camera" className="indent" />   S3 + Cloudfront</Button>
               </Link>
               <Link to="/content/wrapup">
-                <Button id="lid25" onClick={() => this.markComplete("lid25")} className={this.state.lessons.lid25}><FontAwesome name="video-camera" className="indent" />   Wrap up</Button>
+                <Button id="lid25" onClick={() => this.markComplete(25)} className={this.state.lessons.lid25}><FontAwesome name="video-camera" className="indent" />   Wrap up</Button>
               </Link>
             </ButtonGroup>
           </Panel>
@@ -246,13 +226,13 @@ export default class SideBar extends React.Component {
           <Panel collapsible defaultExpanded header="Course Conclusion" className="panel-headings">
             <ButtonGroup fill vertical block>  
               <Link to="/conclusion/wrapup">
-                <Button id="lid26" onClick={() => this.markComplete("lid26")} className={this.state.lessons.lid26}><FontAwesome name="video-camera" className="indent" />   Wrap up</Button>
+                <Button id="lid26" onClick={() => this.markComplete(26)} className={this.state.lessons.lid26}><FontAwesome name="video-camera" className="indent" />   Wrap up</Button>
               </Link>
               <Link to="/conclusion/resources">
-                <Button id="lid27" onClick={() => this.markComplete("lid27")} className={this.state.lessons.lid27}><FontAwesome name="file-text" className="indent" />   Keep Learning</Button>
+                <Button id="lid27" onClick={() => this.markComplete(27)} className={this.state.lessons.lid27}><FontAwesome name="file-text" className="indent" />   Keep Learning</Button>
               </Link>
               <Link to="/conclusion/whatsnext">
-                <Button id="lid28" onClick={() => this.markComplete("lid28")} className={this.state.lessons.lid28}><FontAwesome name="video-camera" className="indent" />   What's Next?</Button>
+                <Button id="lid28" onClick={() => this.markComplete(28)} className={this.state.lessons.lid28}><FontAwesome name="video-camera" className="indent" />   What's Next?</Button>
               </Link>
             </ButtonGroup>
           </Panel>
@@ -260,16 +240,16 @@ export default class SideBar extends React.Component {
           <Panel collapsible defaultExpanded header="Appendix" className="panel-headings">
             <ButtonGroup fill vertical block>  
               <Link to="/appendix/mysql">
-                <Button id="lid29" onClick={() => this.markComplete("lid29")} className={this.state.lessons.lid29}><FontAwesome name="video-camera" className="indent" />   MySQL Refresher</Button>
+                <Button id="lid29" onClick={() => this.markComplete(29)} className={this.state.lessons.lid29}><FontAwesome name="video-camera" className="indent" />   MySQL Refresher</Button>
               </Link>
               <Link to="/appendix/vagrant1">
-                <Button id="lid30" onClick={() => this.markComplete("lid30")} className={this.state.lessons.lid30}><FontAwesome name="video-camera" className="indent" />   Vagrant, Part 1</Button>
+                <Button id="lid30" onClick={() => this.markComplete(30)} className={this.state.lessons.lid30}><FontAwesome name="video-camera" className="indent" />   Vagrant, Part 1</Button>
               </Link>
               <Link to="/appendix/vagrant2">
-                <Button id="lid31" onClick={() => this.markComplete("lid31")} className={this.state.lessons.lid31}><FontAwesome name="video-camera" className="indent" />   Vagrant, Part 2</Button>
+                <Button id="lid31" onClick={() => this.markComplete(31)} className={this.state.lessons.lid31}><FontAwesome name="video-camera" className="indent" />   Vagrant, Part 2</Button>
               </Link>
               <Link to="/appendix/bloopers">
-                <Button id="lid32" onClick={() => this.markComplete("lid32")} className={this.state.lessons.lid32}><FontAwesome name="video-camera" className="indent" />   Bloopers</Button>
+                <Button id="lid32" onClick={() => this.markComplete(32)} className={this.state.lessons.lid32}><FontAwesome name="video-camera" className="indent" />   Bloopers</Button>
               </Link>
             </ButtonGroup>
           </Panel>
