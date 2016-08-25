b52fabf8e516c45e951ea20b44a5c25132d53248
wayneadams <wadams19@gmail.com>
2016-08-25 12:19:59 -0700

    Add images to vertical resources

--- src/vertical/VerticalResources.js
+++ src/vertical/VerticalResources.js
@@ -11,19 +11,15 @@ export default class VerticalResources extends React.Component {
     super(props);
     this.state = {
       markdown: `
-Vertical scaling: buy more power! Upgrade the current machine! Basically, get your hands on more memory, a faster server, etc. This could mean physically buying a new machine if you are running your own hardware setup, or it could mean upgrading your instance in the cloud. 
+Vertical scaling: buy more power! Upgrade the current machine! Basically, get your hands on more memory, a faster server, etc. This could mean physically buying a new machine if you are running your own hardware setup, or it could mean upgrading your instance in the cloud.
 
-> insert
->(diagram of small server upgraded to bigger server --simple)
->
+![MacDown logo](https://s3-us-west-1.amazonaws.com/www.scalabilitysherpa.com/02-+Vertical+Scaling/verticalscalinggraph2.png)
 
 Keep in mind the limiting factors as you work through these exercises. Your buying power decreases drastically as you get beefier and beefier machines: see diagram below of costs. In some situations, the incremental cost is not that big, and the ease of upgrading is justified many times over by the savings in developer time. However, you are limited by $ and by physical limits of today's hardware.
 
->
-> Insert graph of $ vs additional power (incrementally more expensive)
->
+![MacDown logo](https://s3-us-west-1.amazonaws.com/www.scalabilitysherpa.com/02-+Vertical+Scaling/verticalscalinggraph1.png)
 
-We've included links to some appendix material for getting up to speed with Vagrant, a valuable tool to have in your arsenal for easily creating virtual machines. Work through these videos to learn Vagrant and see a basic example of vertical scaling. 
+We've included links to some appendix material for getting up to speed with Vagrant, a valuable tool to have in your arsenal for easily creating virtual machines. Work through these videos to learn Vagrant and see a basic example of vertical scaling.
 
 * [Getting Started with Vagrant, part 1](/#/appendix/vagrant1)
 * [Getting Started with Vagrant, part 2](/#appendix/vagrant2)
@@ -32,8 +28,8 @@ Now, head over to the next video to dive into some code and see vertical scaling
 `
     }
   }
-  
-   
+
+
 
   render() {
     if(!session.loggedIn) {
@@ -41,7 +37,7 @@ Now, head over to the next video to dive into some code and see vertical scaling
         <LockedContent />
       )
     }
-    
+
     return (
       <div className="post">
         <h2>An Introduction to Vertical Scaling</h2>
