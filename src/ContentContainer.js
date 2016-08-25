85d5b21eccdbaf0194f626c9d974768f6f6d615d
Jeremy Toce <jeremytoce@gmail.com>
2016-08-25 15:28:14 -0700

    Fixed previous/next

--- src/ContentContainer.js
+++ src/ContentContainer.js
@@ -3,21 +3,32 @@ import {DropdownButton, MenuItem, ButtonGroup, Button, Col} from 'react-bootstra
 import {Link} from 'react-router';
 
 export default function ContentContainer (props) {
+	
+	var currentLesson = props.currentLesson;
+	console.log(currentLesson)
+	var prevLesson = props.lessonContainer[props.currentLesson-1];
+	var nextLesson = props.lessonContainer[props.currentLesson+1];
+
+	var lessonMinus = (currentLesson - 1) % 33;
+	var lessonPlus = (currentLesson + 1) % 33;
+
 
   return (
 
     <div>
 
-      <Link to={props.lessonContainer[props.currentLesson-1]}>
-        <Button className="prev-next-buttons" onClick={() => props.setCurrentLesson(props.currentLesson-1)}> 
+      <Link to={prevLesson}>
+        <Button className="prev-next-buttons" onClick={() => { props.setCurrentLesson(lessonMinus)}}> 
         <span className="glyphicon glyphicon-menu-left" aria-hidden="true"> </span>   
 			Previous Lesson
 		</Button>
       </Link>
-        <Button className="prev-next-buttons">
+      <Link to={nextLesson}>
+        <Button className="prev-next-buttons" onClick={() => { props.setCurrentLesson(lessonPlus)}}>
          	Next Lesson   
         <span className="glyphicon glyphicon-menu-right" aria-hidden="true"></span>
         </Button>
+        </Link>
     </div>
   )
 }
