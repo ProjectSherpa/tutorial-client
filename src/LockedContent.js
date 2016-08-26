a1d5a78e18cf7657d0608b509e072987e8370e5b
Quin Kinser <quinkinser@gmail.com>
2016-08-26 14:59:38 -0700

    Center videos

--- src/LockedContent.js
+++ src/LockedContent.js
@@ -4,7 +4,7 @@ import {Jumbotron, Col, Row, Button} from 'react-bootstrap';
 
 export default function LockedContent (props) {
   return (
-    <div>
+    <div className="wide-over">
     	<Jumbotron className="lock-jumbotron">
 				<h2 className="locked-text"><span className="glyphicon glyphicon-lock"></span> Lesson Coming Soon!</h2>
 				<Button className="locked-enroll" bsStyle="primary"><h4>Enroll to be Notified of Updates</h4></Button>
